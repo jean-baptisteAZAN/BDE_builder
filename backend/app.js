aab33965a310994a3915dc77a94c9a74c08549cf
jeanbaptisteAZAN <azan.jean-baptiste@epitech.eu>
2024-06-13 15:16:25 +0200

    feat(backend):secure

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
@@ -6,7 +7,18 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(cors({ origin: "http://192.168.0.14:5173" }));
 
-const serviceAccount = require("./firebasekey.json");
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.FIREBASE_CLIENT_EMAIL}`
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -31,7 +43,6 @@ app.get("/api/userinfo", async (req, res) => {
     const decodedToken = await admin.auth().verifyIdToken(token);
     const uid = decodedToken.uid;
 
-    // Récupérer les informations de l'utilisateur depuis Firestore
     const userDoc = await db.collection("users").doc(uid).get();
 
     if (!userDoc.exists) {
